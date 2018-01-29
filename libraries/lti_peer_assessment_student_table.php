a18968d7ab75fc9a104da03d3095dcb64fd048fc
Paul Sijpkes <ps158@boldapp.newcastle.edu.au>
2018-01-29 11:29:23 +1100

    remove sample users from total student count

--- libraries/lti_peer_assessment_student_table.php
+++ libraries/lti_peer_assessment_student_table.php
@@ -1,21 +1,27 @@
 <?php
+$sample_members = ee()->config->item('sample_members');
+$sample_length = count($sample_members["ids"]);
+
 ee()->db->where(array("lti_peer_assessments.resource_link_id" => $resource_link_id));
 ee()->db->join("lti_group_contexts", "lti_group_contexts.member_id = lti_peer_assessments.assessor_member_id");
 ee()->db->group_by("assessor_member_id");
 $res = ee()->db->get("lti_peer_assessments");
-$total_students = $res->num_rows();
+$total_students = $res->num_rows() - $sample_length;
 $vars['lti_peer_assessment']['total_students'] = array("label" => "Total Students: ",  "value" => $total_students);
 
 $completed_peer_assessment = 0;
 $id = array();
 foreach($res->result_array() as $row) {
+
     ee()->db->where(array("assessor_member_id" => $row['assessor_member_id'], "locked" => '1', "resource_link_id" => $resource_link_id));
     ee()->db->join("lti_group_contexts", "lti_group_contexts.member_id = lti_peer_assessments.assessor_member_id");
     ee()->db->group_by("assessor_member_id");
     $res = ee()->db->get('lti_peer_assessments');
     if($res->num_rows() > 0) {
-        ++$completed_peer_assessment;
-        $ids[] = $row['assessor_member_id'];
+        if(! in_array($row['assessor_member_id'], $sample_members["ids"])) {
+            ++$completed_peer_assessment;
+            $ids[] = $row['assessor_member_id'];
+        }
     }
 }
 
