1fe0bde452ffea3db025dbbcf2951e96b533a77e
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-05-07 14:53:52 +1000

    add clear filter

--- js/lti_peer_assessment_student_table.js
+++ js/lti_peer_assessment_student_table.js
@@ -195,7 +195,7 @@ $(document).ready(function() {
 
     $('button#clear_filters').bind("click", function(e) {
         e.preventDefault();
-        $('input[name=filter_submitted], input#st_search').prop('checked', false);
-         $("form#filters").submit();
+          $('input[name=filter_submitted]').prop('checked', false);
+          document.location.reload();
     });
 });
