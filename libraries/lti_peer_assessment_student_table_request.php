258b68ea1adf42697aab7c4ded1cbe4b2ec9daf1
Paul Sijpkes <paul.sijpkes@gmail.com>
2018-05-07 15:17:43 +1000

    clear filter cookie

--- libraries/lti_peer_assessment_student_table_request.php
+++ libraries/lti_peer_assessment_student_table_request.php
@@ -2,6 +2,9 @@
 if(isset($_COOKIE["filter_submitted"]) && !empty($_COOKIE["filter_submitted"])) {
     $plugin_filters["filter_submitted"] = $_COOKIE["filter_submitted"];
 }
+if(empty($_POST['filter_submitted'])) {
+    unset($_COOKIE["filter_submitted"]);
+}
 if(isset($_POST['filter_submitted']) && !empty($_POST['filter_submitted'])) {
     $plugin_filters["filter_submitted"] = $_POST['filter_submitted'];
 
